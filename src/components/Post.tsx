25663de5ecd6b4ad44ccc8eda7452619f46d0688
MarkSi1986 <simmondsmark@ymail.com>
2025-07-09 14:23:49 +0100

    Initial development

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,21 +1,31 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Spinner, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Post() {
-
     const [posts, setPosts] = useState(null);
 
     useEffect(() => {
         axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                setPosts(response.data);
+                console.log(response.data);
+            });
+    }, []);
 
-        .then(response => {
-            setPosts(response.data)
-            console.log(response.data);
-        })
-    },[])
+    if (posts === null) {
+        return (
+            <Container fluid className="vh-100 d-flex justify-content-center align-items-center">
+                <Spinner animation="border" variant="primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Container>
+        );
+    }
 
     return (
-        <div>
+        <div className="p-4">
             <table className="table table-striped table-bordered">
                 <thead>
                     <tr>
@@ -26,16 +36,16 @@ export default function Post() {
                     </tr>
                 </thead>
                 <tbody>
-                    {posts !== null? posts.map(post => (
-                        <tr>
-                            <td>{post.userID}</td>
+                    {posts.map(post => (
+                        <tr key={post.id}>
                             <td>{post.id}</td>
+                            <td>{post.userId}</td>
                             <td>{post.title}</td>
+                            <td>–</td>
                         </tr>
-                    )): (<div>...loading</div>)}
+                    ))}
                 </tbody>
             </table>
         </div>
-    )
-
+    );
 }
